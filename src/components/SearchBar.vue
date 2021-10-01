76facda48c616306d057cd4de52190146aaee938
sofia-papa <papa.sofia@outlook.it>
2021-10-01 16:31:47 +0100

    start repository

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,29 @@
+<template>
+      <form>
+          <div>
+              <input type="text" placeholder="digita il nome del film">
+          </div>
+
+          <div>
+              <button>Cerca</button>
+          </div>
+      </form>
+  
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: "SearchBar",
+    data: function(){
+
+    }
+
+}
+</script>
+
+<style scoped lang="scss">
+@import './style/variables.scss';
+
+</style>
